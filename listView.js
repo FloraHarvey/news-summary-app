11f9ad3e91db82c34bd6096377d480fccc7fc853
Flora Harvey <fharvey555@gmail.com>
2017-04-01 19:30:22 +0100

    Listview generates HTML for list of headlines

--- listView.js
+++ listView.js
@@ -6,5 +6,11 @@
     this.list = list;
   }
 
+ListView.prototype.generateHeadlineHtml = function () {
+  return this.list.viewArticles().map(function(article) {
+    return "<ul><li>" + article.headline + "</li></ul>";
+  }).join();
+};
+
   exports.ListView = ListView;
 })(this);
--- spec/listViewSpec.js
+++ spec/listViewSpec.js
@@ -11,5 +11,18 @@ function listViewCanBeInstantiatedWithList () {
   }
 }
 
+function listViewCanGenerateHtml () {
+  var list = new List();
+  var listView = new ListView(list);
+  list.addArticle("News", "Warmest day of the year...");
+  try {
+    new Assert(listView.generateHeadlineHtml() === "<ul><li>News</li></ul>", "listView did not generate HTML", "listViewCanGenerateHtml").isTrue();
+  }
+  catch(err) {
+    console.log(err.message);
+  }
+}
+
 
 listViewCanBeInstantiatedWithList();
+listViewCanGenerateHtml();
